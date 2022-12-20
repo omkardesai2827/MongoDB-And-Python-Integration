4e7d24e8ff68d327ce1e007864c3ed3fe2fe177b
Omkar Desai <desaiomkar767@gmail.com>
2022-12-20 21:07:17 +0530

    Add files via upload

---
+++ Task2_Telephone Directory.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = client ["mytaskdatabase"] 
+mycol = mydb ["Telephone"] 
+document = { "Name": "Omkar", "Phone number": "123-456-7890", "Place": "New York" }
+x = mycol.insert_one(document)
+for x in mycol.find():
+    print (x)
+query = { "Name": "Omkar" }
+result = mycol.find_one(query)
+
+print(result)
+update = { "$set": { "Place": "Toronto" } }
+result = mycol.update_one(query, update)
+
+print(result.modified_count)
+
+
+# In[ ]:
+
+
+
+
